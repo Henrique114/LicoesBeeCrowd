206c9904cb11c152598ad334355099e34eb05483
Henrique114 <h.ferreira114@gmail.com>
2025-01-24 01:04:15 -0300

    Primeiro commit

---
+++ LicoesBcrowd1014/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LicoesBcrowd1014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcule o consumo médio de um automóvel sendo
+            //fornecidos a distância total percorrida (em Km)
+            //e o total de combustível gasto (em litros).
+
+            var distanciaPercorrida  =  Int32.Parse(Console.ReadLine());    
+            var litrosGastos = float.Parse(Console.ReadLine());
+
+            var consumo = distanciaPercorrida/litrosGastos;
+
+            Console.WriteLine($"{consumo:F3} km/l.");
+
+        }
+    }
+}
